57f529336db432f1e96aac5a457b6b263718a49b
Julio Cesar- intelamd <113316441+gushabo@users.noreply.github.com>
2023-02-15 08:48:41 -0600

    001_Gustavo
    Se realizo la primera practica de "Hola Mundo".

---
+++ 2023_02_15_001_HolaMundo_V0/2023_02_15_001_HolaMundo_V0.cpp
@@ -0,0 +1,14 @@
+// 2023_02_15_001_HolaMundo_V0.cpp 
+// Este archivo es mi primer programa
+// Gustavo Galindo Velazquez zap843
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Resiste y sobrevive" << endl;
+    cout << "hemos tenido suerte y esa suerte en algun punto se va a terminar" << endl;
+}
